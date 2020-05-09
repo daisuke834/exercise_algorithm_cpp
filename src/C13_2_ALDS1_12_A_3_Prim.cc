b004057cad32e469e945b3100084f9701a4a2379
Daisuke Hashimoto <14502049+daisuke834@users.noreply.github.com>
2020-05-09 17:18:57 +0900

    ALDS1_12_A Implemented MST using Prim algorithm and priority queue

--- src/C13_2_ALDS1_12_A_3_Prim.cc
+++ src/C13_2_ALDS1_12_A_3_Prim.cc
@@ -1,13 +1,11 @@
 /**
  * @copyright (c) 2020 Daisuke Hashimoto
  * @brief Part13-2. 重み付きグラフ(Weighted Graph)、最小全域木(Minimum Spanning Tree, MST)。
- * プリム法 (Prim's algorithm)による実装。計算量はO(|V|^2)。
+ * プリム法 (Prim's algorithm)と優先度付きキューによる実装。計算量はO((V+E)*log(V))。
  * 「アルゴリズムとデータ構造」(渡部有隆(著))のアルゴリズムのスクラッチ実装。
  */
 
 #include "src/C13_2_ALDS1_12_A_3_Prim.h"
-#include <iostream>
-#include <string>
 
 namespace ALDS1_12_A_3 {
 
@@ -19,14 +17,18 @@ void CallPrimAlgorithm(std::istream &input_stream) {
     int32_t number_of_vertices;
     input_stream >> number_of_vertices;
     minimum_spanning_tree->SetNumberOfVertices(number_of_vertices);
-    for (int32_t vertex_from = 0; vertex_from < number_of_vertices; ++vertex_from) {
-      for (int32_t vertex_to = 0; vertex_to < number_of_vertices; ++vertex_to) {
+    for (int32_t from = 0; from < number_of_vertices; ++from) {
+      for (int32_t to = 0; to < number_of_vertices; ++to) {
         int32_t weight;
         input_stream >> weight;
-        minimum_spanning_tree->AddWeight(vertex_from, vertex_to, weight);
+        if (weight >= 0) {
+          minimum_spanning_tree->AddVertex(from, to, weight);
+        }
       }
     }
-    const int32_t sum_of_weights = minimum_spanning_tree->CalculateSumOfWeightsOfPrimAlgorithm();
+    constexpr int32_t kStartVertex = 0;
+    minimum_spanning_tree->BuildMinimumSpanningTree(kStartVertex);
+    const int32_t sum_of_weights = minimum_spanning_tree->GetSumOfWeightsOfMst();
     std::cout << sum_of_weights << std::endl;
   } catch (...) {
     std::cerr << "ERROR: CallPrimAlgorithm()" << std::endl;
@@ -36,6 +38,48 @@ void CallPrimAlgorithm(std::istream &input_stream) {
 }
 
 // ****************************************************
+PrimAlgorithm::PrimAlgorithm() noexcept : number_of_vertices_(0), sum_of_weights_(0) {}
+
+PrimAlgorithm::~PrimAlgorithm() noexcept {}
+
+void PrimAlgorithm::SetNumberOfVertices(const int32_t number_of_vertices) noexcept {
+  number_of_vertices_ = number_of_vertices;
+}
+
+void PrimAlgorithm::AddVertex(const int32_t from, const int32_t to, const int32_t weight) noexcept {
+  Edge edge1;
+  edge1.to = to;
+  edge1.weight = weight;
+  vertices_[from].neighbours.push_back(edge1);
+}
+
+void PrimAlgorithm::BuildMinimumSpanningTree(const int32_t start_vertex_index) noexcept {
+  constexpr int32_t kZeroDistance = 0;
+  priority_queue_.push(Pair(kZeroDistance, start_vertex_index));
+  while (priority_queue_.size() > 0) {
+    const int32_t c_distance = priority_queue_.top().first;
+    const int32_t c_vertex_index = priority_queue_.top().second;
+    priority_queue_.pop();
+    if (!vertices_[c_vertex_index].assigned_to_tree) {
+      AddToTree(c_vertex_index, c_distance);
+      for (const Edge &c_edge : vertices_[c_vertex_index].neighbours) {
+        const int32_t neighbour_index = c_edge.to;
+        if (!vertices_[neighbour_index].assigned_to_tree) {
+          priority_queue_.push(Pair(c_edge.weight, neighbour_index));
+        }
+      }
+    }
+  }
+}
+
+int32_t PrimAlgorithm::GetSumOfWeightsOfMst() const noexcept {
+  return sum_of_weights_;
+}
+
+void PrimAlgorithm::AddToTree(const int32_t child, const int32_t weight) noexcept {
+  vertices_[child].assigned_to_tree = true;
+  sum_of_weights_ += weight;
+}
 
 // **********************************************************************
 
--- src/C13_2_ALDS1_12_A_3_Prim.h
+++ src/C13_2_ALDS1_12_A_3_Prim.h
@@ -1,21 +1,58 @@
 /**
  * @copyright (c) 2020 Daisuke Hashimoto
  * @brief Part13-2. 重み付きグラフ(Weighted Graph)、最小全域木(Minimum Spanning Tree, MST)。
- * プリム法 (Prim's algorithm)による実装。
+ * プリム法 (Prim's algorithm)と優先度付きキューによる実装。計算量はO((V+E)*log(V))。
  * 「アルゴリズムとデータ構造」(渡部有隆(著))のアルゴリズムのスクラッチ実装。
  */
 
 #ifndef SRC_C13_2_ALDS1_12_A_3_MINIMUM_SPANNING_TREE_H_
 #define SRC_C13_2_ALDS1_12_A_3_MINIMUM_SPANNING_TREE_H_
 
-#include <gtest/gtest_prod.h>  // Needed for FRIEND_TEST. Should be removed from production code
+#include <algorithm>
 #include <cstdint>
+#include <functional>
 #include <iostream>
+#include <queue>
 #include <string>
+#include <utility>
+#include <vector>
 
 namespace ALDS1_12_A_3 {
 
+constexpr int32_t kMaxNumberOfVertices = 100;
+constexpr int32_t kInvalidIndex = -1;
+
 void CallPrimAlgorithm(std::istream &input_stream);
+
+struct Edge {
+  int32_t to{kInvalidIndex};
+  int32_t weight{INT32_MAX};
+};
+
+struct Vertex {
+  bool assigned_to_tree{false};
+  std::vector<Edge> neighbours;
+};
+
+class PrimAlgorithm {
+ public:
+  PrimAlgorithm() noexcept;
+  ~PrimAlgorithm() noexcept;
+  void SetNumberOfVertices(const int32_t number_of_vertices) noexcept;
+  void AddVertex(const int32_t from, const int32_t to, const int32_t weight) noexcept;
+  void BuildMinimumSpanningTree(const int32_t start_vertex_index) noexcept;
+  int32_t GetSumOfWeightsOfMst() const noexcept;
+
+ private:
+  void AddToTree(const int32_t child, const int32_t weight) noexcept;
+
+ private:
+  int32_t number_of_vertices_;
+  int32_t sum_of_weights_;
+  Vertex vertices_[kMaxNumberOfVertices];
+  using Pair = std::pair<int32_t, int32_t>;
+  std::priority_queue<Pair, std::vector<Pair>, std::greater<Pair>> priority_queue_;
+};
 
 }  // namespace ALDS1_12_A_3
 
