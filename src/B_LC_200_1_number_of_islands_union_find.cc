57eb407b3521f2f50c7a56a87ab0f3d8cb123059
Daisuke Hashimoto <14502049+daisuke834@users.noreply.github.com>
2020-05-13 14:38:19 +0900

    LC_200 Modified comments

--- src/B_LC_200_1_number_of_islands_union_find.cc
+++ src/B_LC_200_1_number_of_islands_union_find.cc
@@ -1,6 +1,6 @@
 /**
  * @copyright (c) 2020 Daisuke Hashimoto
- * @brief LeetCode 200. Number of Islands
+ * @brief LeetCode 200. Number of Islands. Implemented by Union Find Trees.
  * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. 
  * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
  * You may assume all four edges of the grid are all surrounded by water.
