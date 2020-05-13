4a1c031968323f87b3a0c6a861eb3cdbe781dce1
Daisuke Hashimoto <14502049+daisuke834@users.noreply.github.com>
2020-05-14 01:42:51 +0900

    LC_200 Implemented Number of Islands problem by Breadth First Search (BFS)

---
+++ test/test_B_LC_200_3_number_of_islands_bfs.cc
@@ -0,0 +1,84 @@
+/**
+ * @copyright (c) 2020 Daisuke Hashimoto
+ */
+
+#include <gtest/gtest.h>
+#include <gtest/internal/gtest-port.h>
+#include <iostream>
+#include <sstream>
+#include "src/B_LC_200_3_number_of_islands_bfs.h"
+
+namespace LC_200_3 {
+
+class Test_LC_200_3 : public ::testing::Test {
+ protected:
+  virtual void SetUp() {}
+  virtual void TearDown() {}
+};
+
+TEST_F(Test_LC_200_3, CallNumberOfIsland_1) {
+  std::ostringstream answer;
+  answer << "1" << std::endl;
+
+  std::ostringstream input_stream;
+  input_stream << "11110" << std::endl;
+  input_stream << "11010" << std::endl;
+  input_stream << "11000" << std::endl;
+  input_stream << "00000" << std::endl;
+  std::istringstream iss(input_stream.str());
+  testing::internal::CaptureStdout();
+  bool success = true;
+  try {
+    CallNumberOfIslands(iss);
+  } catch (...) {
+    success = false;
+  }
+  const std::string captured_stdout = testing::internal::GetCapturedStdout();
+  EXPECT_TRUE(success);
+  ASSERT_STREQ(answer.str().c_str(), captured_stdout.c_str());
+}
+
+TEST_F(Test_LC_200_3, CallNumberOfIsland_2) {
+  std::ostringstream answer;
+  answer << "3" << std::endl;
+
+  std::ostringstream input_stream;
+  input_stream << "11000" << std::endl;
+  input_stream << "11000" << std::endl;
+  input_stream << "00100" << std::endl;
+  input_stream << "00011" << std::endl;
+  std::istringstream iss(input_stream.str());
+  testing::internal::CaptureStdout();
+  bool success = true;
+  try {
+    CallNumberOfIslands(iss);
+  } catch (...) {
+    success = false;
+  }
+  const std::string captured_stdout = testing::internal::GetCapturedStdout();
+  EXPECT_TRUE(success);
+  ASSERT_STREQ(answer.str().c_str(), captured_stdout.c_str());
+}
+
+TEST_F(Test_LC_200_3, CallNumberOfIsland_3) {
+  std::ostringstream answer;
+  answer << "1" << std::endl;
+
+  std::ostringstream input_stream;
+  input_stream << "111" << std::endl;
+  input_stream << "010" << std::endl;
+  input_stream << "111" << std::endl;
+  std::istringstream iss(input_stream.str());
+  testing::internal::CaptureStdout();
+  bool success = true;
+  try {
+    CallNumberOfIslands(iss);
+  } catch (...) {
+    success = false;
+  }
+  const std::string captured_stdout = testing::internal::GetCapturedStdout();
+  EXPECT_TRUE(success);
+  ASSERT_STREQ(answer.str().c_str(), captured_stdout.c_str());
+}
+
+}  // namespace LC_200_3
