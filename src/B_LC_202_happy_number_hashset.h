562f6ef116e1537b38e7edc7a0a8248e3790a183
Daisuke Hashimoto <14502049+daisuke834@users.noreply.github.com>
2020-05-14 17:59:28 +0900

    LC_202 Implemented Happy Number problem by using HashMap (unordered_map)

---
+++ src/B_LC_202_happy_number_hashset.h
@@ -0,0 +1,24 @@
+/**
+ * @copyright (c) 2020 Daisuke Hashimoto
+ * @brief LeetCode 202. Happy Number. HashSetを用いた解法
+ */
+
+#ifndef SRC_B_LC_202_HAPPY_NUMBER_H_
+#define SRC_B_LC_202_HAPPY_NUMBER_H_
+
+#include <cstdint>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+namespace LC_202 {
+
+void CallHappyNumber(std::istream &input_stream) noexcept;
+
+bool IsHappy(const int32_t n) noexcept;
+
+int32_t SumOfSquaresOfEachDigit(const int32_t n) noexcept;
+
+}  // namespace LC_202
+
+#endif  // SRC_B_LC_202_HAPPY_NUMBER_H_
