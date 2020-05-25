75c27f2d48d595c4aecd2b226d74fa1141f240d4
Daisuke Hashimoto <14502049+daisuke834@users.noreply.github.com>
2020-05-26 00:35:03 +0900

    AGC_044 Implemented AGC_044_B by BFS (WIP)

--- src/D_AGC_044_B_joker.h
+++ src/D_AGC_044_B_joker.h
@@ -8,10 +8,19 @@
 
 #include <cstdint>
 #include <iostream>
+#include <queue>
 #include <string>
+#include <utility>
 #include <vector>
 
 namespace AGC_044_B {
+
+using Position = std::pair<int32_t, int32_t>;  // row, column
+
+struct Audience {
+  bool exited{false};
+  int32_t distance_to_exit{INT32_MAX};
+};
 
 class Solution {
  public:
@@ -20,6 +29,21 @@ class Solution {
   int32_t CalculateNumberOfPairs(const std::vector<int32_t> &people_to_exit) noexcept;
 
  private:
+  void ExitOneAudience(const Position &position) noexcept;
+  Position GetPosition(const int32_t audience_number) const noexcept;
+  void UpdateDistance(const Position &position, const int32_t distance) noexcept;
+  void UpdateAllOfDistances() noexcept;
+  int32_t GetDistanceWhenEmpty(const Position &position) const noexcept;
+  bool IsValidPosition(const Position &position) const noexcept;
+  bool IsEmpty(const Position &position) const noexcept;
+  int32_t GetDistance(const Position &position) const noexcept;
+  void PrintDebug() const noexcept;
+
+ private:
+  int32_t N_;
+  int32_t number_of_dislike_;
+  std::vector<std::vector<Audience>> audiences_;
+  std::queue<Position> q_;
 };
 
 }  // namespace AGC_044_B
