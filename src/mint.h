59ebcbec9cf56fb18cbc2956b20c43751ca7ebc3
Daisuke Hashimoto <14502049+daisuke834@users.noreply.github.com>
2020-06-02 13:08:00 +0900

    Added mint library

---
+++ src/mint.h
@@ -0,0 +1,74 @@
+/**
+ * @copyright (c) 2020 Daisuke Hashimoto
+ * @brief MOD INT
+ */
+
+#ifndef SRC_MINT_H_
+#define SRC_MINT_H_
+
+#include <cstdint>
+#include <iostream>
+
+namespace mint {
+
+const int64_t kMod = 1000000007LL;
+// const int kMod = 998244353LL;
+struct Mint {
+  int64_t value;
+  Mint(int64_t value = 0LL) : value((value % kMod + kMod) % kMod) {}
+  Mint operator-() const {
+    return Mint(-value);
+  }
+  Mint &operator+=(const Mint rhs) {
+    if ((value += rhs.value) >= kMod)
+      value -= kMod;
+    return *this;
+  }
+  Mint &operator-=(const Mint rhs) {
+    if ((value += kMod - rhs.value) >= kMod)
+      value -= kMod;
+    return *this;
+  }
+  Mint &operator*=(const Mint rhs) {
+    (value *= rhs.value) %= kMod;
+    return *this;
+  }
+  Mint operator+(const Mint rhs) const {
+    return Mint(*this) += rhs;
+  }
+  Mint operator-(const Mint rhs) const {
+    return Mint(*this) -= rhs;
+  }
+  Mint operator*(const Mint rhs) const {
+    return Mint(*this) *= rhs;
+  }
+  Mint Pow(int64_t p) const {
+    if (p == 0) {
+      return 1LL;
+    } else if (p < 0) {
+      return Pow(-p).Pow(kMod - 2);
+    }
+    Mint temp = Pow(p >> 1LL);
+    temp *= temp;
+    if (p & 1) {
+      temp *= (*this);
+    }
+    return temp;
+  }
+  Mint Inv() const {
+    return Pow(kMod - 2);
+  }
+  Mint &operator/=(const Mint a) {
+    return *this *= a.Inv();
+  }
+  Mint operator/(const Mint a) const {
+    return Mint(*this) /= a;
+  }
+};
+std::ostream &operator<<(std::ostream &os, const Mint &a) {
+  os << a.value;
+  return os;
+}
+
+}  // namespace mint
+#endif  // SRC_MINT_H_
