70cbf088588f7533c1af05be86c11283cd9e9caa
Daisuke Hashimoto <14502049+daisuke834@users.noreply.github.com>
2020-06-14 15:36:53 +0900

    LC_weekly_193_3 Binary Search

---
+++ src/B_LC_weekly_193_3_binary_search.h
@@ -0,0 +1,27 @@
+/**
+ * @copyright (c) 2020 Daisuke Hashimoto
+ * @brief LeetCode weekly-193-3. 5455. Minimum Number of Days to Make m Bouquets.
+ */
+
+#ifndef SRC_B_LC_WEEKLY_189_3_SUBSEQUENCE_H_
+#define SRC_B_LC_WEEKLY_189_3_SUBSEQUENCE_H_
+
+#include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace LC_weekly_193_3 {
+
+class Solution {
+ public:
+  int minDays(std::vector<int> &bloomDay, int m, int k);
+
+ private:
+  bool IsOkay(const std::vector<int> &bloomDay, const int m, const int k, const int day);
+};
+
+}  // namespace LC_weekly_193_3
+
+#endif  // SRC_B_LC_WEEKLY_189_3_SUBSEQUENCE_H_
