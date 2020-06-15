b3e888bf251c3bbad3364c12c8a8262a5c68ab59
Daisuke Hashimoto <14502049+daisuke834@users.noreply.github.com>
2020-06-15 16:26:36 +0900

    D_ABC_170 E - Smart Infants using multiset

---
+++ src/D_ABC_170_E_multiset.h
@@ -0,0 +1,18 @@
+/**
+ * @copyright (c) 2020 Daisuke Hashimoto
+ * @brief AtCoder Beginner Contest(ABC) 170-E. Smart Infants.
+ */
+
+#ifndef SRC_D_D_ABC_170_E_MULTISET_H_
+#define SRC_D_D_ABC_170_E_MULTISET_H_
+
+#include <cstdint>
+#include <iostream>
+
+namespace D_ABC_170_E {
+
+int CallMain(std::istream &input_stream);
+
+}  // namespace D_ABC_170_E
+
+#endif  // SRC_D_D_ABC_170_E_MULTISET_H_
