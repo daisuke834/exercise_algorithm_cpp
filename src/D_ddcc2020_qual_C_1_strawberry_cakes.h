37e9a28a952982c04dc92b05487fbba5f096f315
Daisuke Hashimoto <14502049+daisuke834@users.noreply.github.com>
2020-06-17 20:35:49 +0900

    D_ddcc2020_qual_C Strawberry Cakes

---
+++ src/D_ddcc2020_qual_C_1_strawberry_cakes.h
@@ -0,0 +1,19 @@
+/**
+ * @copyright (c) 2020 Daisuke Hashimoto
+ * @brief AtCoder DISCO presents ディスカバリーチャンネル コードコンテスト2020 予選
+ * C. Strawberry Cakes.
+ */
+
+#ifndef SRC_D_DDCC2020_QUAL_C_1_MULTISET_H_
+#define SRC_D_DDCC2020_QUAL_C_1_MULTISET_H_
+
+#include <cstdint>
+#include <iostream>
+
+namespace D_ddcc2020_qual_C_1 {
+
+int CallMain(std::istream &input_stream);
+
+}  // namespace D_ddcc2020_qual_C_1
+
+#endif  // SRC_D_DDCC2020_QUAL_C_1_MULTISET_H_
---
+++ test/test_D_ddcc2020_qual_C_1_strawberry_cakes.cc
@@ -0,0 +1,25 @@
+/**
+ * @copyright (c) 2020 Daisuke Hashimoto
+ * @brief AtCoder DISCO presents ディスカバリーチャンネル コードコンテスト2020 予選
+ * C. Strawberry Cakes.
+ */
+
+#include <gtest/gtest.h>
+#include <gtest/internal/gtest-port.h>
+#include <iostream>
+#include <sstream>
+#include "src/D_ddcc2020_qual_C_1_strawberry_cakes.h"
+
+namespace D_ddcc2020_qual_C_1 {
+
+class Test_D_ddcc2020_qual_C_1 : public ::testing::Test {
+ protected:
+  virtual void SetUp() {}
+  virtual void TearDown() {}
+};
+
+TEST_F(Test_D_ddcc2020_qual_C_1, test_1) {
+  ASSERT_TRUE(true);
+}
+
+}  // namespace D_ddcc2020_qual_C_1
