21af2430e3d45bf0803734f88a940ff9838ab08e
Daisuke Hashimoto <14502049+daisuke834@users.noreply.github.com>
2020-06-21 00:20:12 +0900

    D_ABC_147 C. HonestOrUnkind2 : Bit search

---
+++ src/D_ABC_147_C_bit_search.h
@@ -0,0 +1,18 @@
+/**
+ * @copyright (c) 2020 Daisuke Hashimoto
+ * @brief AtCoder Beginner Contest(ABC) 147-C. ビット全探索.
+ */
+
+#ifndef SRC_D_DDCC2020_QUAL_C_2_STRAWBERRY_H_
+#define SRC_D_DDCC2020_QUAL_C_2_STRAWBERRY_H_
+
+#include <cstdint>
+#include <iostream>
+
+namespace D_ABC_147_C {
+
+int CallMain(std::istream &input_stream);
+
+}  // namespace D_ABC_147_C
+
+#endif  // SRC_D_DDCC2020_QUAL_C_2_STRAWBERRY_H_
