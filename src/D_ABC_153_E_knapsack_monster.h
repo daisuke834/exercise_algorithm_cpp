d0d65f01729c040a198d7803456dd49e5a29f5c5
Daisuke Hashimoto <14502049+daisuke834@users.noreply.github.com>
2020-06-22 22:57:38 +0900

    ABC_153_E AtCoder Beginner Contest(ABC) 153-E. Crested Ibis vs Monster.

---
+++ src/D_ABC_153_E_knapsack_monster.h
@@ -0,0 +1,18 @@
+/**
+ * @copyright (c) 2020 Daisuke Hashimoto
+ * @brief AtCoder Beginner Contest(ABC) 153-E. Crested Ibis vs Monster.
+ */
+
+#ifndef SRC_D_ABC_153_E_MULTISET_H_
+#define SRC_D_ABC_153_E_MULTISET_H_
+
+#include <cstdint>
+#include <iostream>
+
+namespace ABC_153_E {
+
+int CallMain(std::istream &input_stream);
+
+}  // namespace ABC_153_E
+
+#endif  // SRC_D_ABC_153_E_MULTISET_H_
