eeabfaddcf75d8fe495b4db3894a75adad18c069
Daisuke Hashimoto <14502049+daisuke834@users.noreply.github.com>
2020-06-28 19:02:50 +0900

    LC_weekly_195_4 1499. Max Value of Equation

---
+++ src/B_LC_weekly_195_4_two_pointers.h
@@ -0,0 +1,25 @@
+/**
+ * @copyright (c) 2020 Daisuke Hashimoto
+ * @brief LeetCode weekly-195-4. 1499. Max Value of Equation.
+ * https://leetcode.com/contest/weekly-contest-195/problems/max-value-of-equation/
+ */
+
+#ifndef SRC_B_LC_WEEKLY_195_4_TWO_POINTERS_H_
+#define SRC_B_LC_WEEKLY_195_4_TWO_POINTERS_H_
+
+#include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace LC_weekly_195_4 {
+
+class Solution {
+ public:
+  int findMaxValueOfEquation(std::vector<std::vector<int>> &points, int k);
+};
+
+}  // namespace LC_weekly_195_4
+
+#endif  // SRC_B_LC_WEEKLY_195_4_TWO_POINTERS_H_
