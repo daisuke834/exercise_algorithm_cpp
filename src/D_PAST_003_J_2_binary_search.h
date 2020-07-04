65fc0fa55e63feb55fac2473b5dad1b3be167959
Daisuke Hashimoto <14502049+daisuke834@users.noreply.github.com>
2020-07-04 17:27:47 +0900

    PAST_003_J Binary search

---
+++ src/D_PAST_003_J_2_binary_search.h
@@ -0,0 +1,18 @@
+/**
+ * @copyright (c) 2020 Daisuke Hashimoto
+ * @brief AtCoder Practical Algorithm Skill Test (PAST) 003-J. 回転寿司.
+ */
+
+#ifndef SRC_D_PAST_003_J_2_UPPER_BOUND_H_
+#define SRC_D_PAST_003_J_2_UPPER_BOUND_H_
+
+#include <cstdint>
+#include <iostream>
+
+namespace PAST_003_J_2 {
+
+int CallMain(std::istream &input_stream);
+
+}  // namespace PAST_003_J_2
+
+#endif  // SRC_D_PAST_003_J_2_UPPER_BOUND_H_
