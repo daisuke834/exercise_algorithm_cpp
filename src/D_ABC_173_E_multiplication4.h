51457e167fb2e0e3dadcc3cefd7d9b743c9ffe47
Daisuke Hashimoto <14502049+daisuke834@users.noreply.github.com>
2020-07-06 23:39:00 +0900

    D_ABC_173_E ABC 173-E. Multiplication 4

---
+++ src/D_ABC_173_E_multiplication4.h
@@ -0,0 +1,18 @@
+/**
+ * @copyright (c) 2020 Daisuke Hashimoto
+ * @brief AtCoder Beginner Contest(ABC) 173-E. Multiplication 4.
+ */
+
+#ifndef SRC_D_D_ABC_173_E_MULTIPLICATION_H_
+#define SRC_D_D_ABC_173_E_MULTIPLICATION_H_
+
+#include <cstdint>
+#include <iostream>
+
+namespace D_ABC_173_E {
+
+int CallMain(std::istream &input_stream);
+
+}  // namespace D_ABC_173_E
+
+#endif  // SRC_D_D_ABC_173_E_MULTIPLICATION_H_
