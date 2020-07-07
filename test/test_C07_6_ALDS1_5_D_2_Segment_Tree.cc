abd266756577f68b3487062b80d2ecd3d9743770
Daisuke Hashimoto <14502049+daisuke834@users.noreply.github.com>
2020-07-08 02:07:16 +0900

    ALDS1_5_D Added test cases

--- test/test_C07_6_ALDS1_5_D_2_Segment_Tree.cc
+++ test/test_C07_6_ALDS1_5_D_2_Segment_Tree.cc
@@ -61,4 +61,46 @@ TEST_F(Test_ALDS1_5_D_2, NumberOfInversions_2) {
   ASSERT_STREQ(answer.str().c_str(), captured_stdout.c_str());
 }
 
+TEST_F(Test_ALDS1_5_D_2, NumberOfInversions_3) {
+  std::ostringstream answer;
+  answer << "12" << std::endl;
+
+  std::ostringstream input_stream;
+  input_stream << "8" << std::endl;
+  input_stream << "6 1 5 8 2 3 4 7" << std::endl;
+
+  std::istringstream iss(input_stream.str());
+  testing::internal::CaptureStdout();
+  bool success = true;
+  try {
+    CallMain(iss);
+  } catch (...) {
+    success = false;
+  }
+  const std::string captured_stdout = testing::internal::GetCapturedStdout();
+  EXPECT_TRUE(success);
+  ASSERT_STREQ(answer.str().c_str(), captured_stdout.c_str());
+}
+
+TEST_F(Test_ALDS1_5_D_2, NumberOfInversions_4) {
+  std::ostringstream answer;
+  answer << "123" << std::endl;
+
+  std::ostringstream input_stream;
+  input_stream << "23" << std::endl;
+  input_stream << "69 29 30 5 17 93 19 32 68 66 33 62 43 52 47 56 0 23 51 11 72 80 8" << std::endl;
+
+  std::istringstream iss(input_stream.str());
+  testing::internal::CaptureStdout();
+  bool success = true;
+  try {
+    CallMain(iss);
+  } catch (...) {
+    success = false;
+  }
+  const std::string captured_stdout = testing::internal::GetCapturedStdout();
+  EXPECT_TRUE(success);
+  ASSERT_STREQ(answer.str().c_str(), captured_stdout.c_str());
+}
+
 }  // namespace ALDS1_5_D_2
