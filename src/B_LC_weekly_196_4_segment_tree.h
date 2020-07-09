890fd685304148bbd995e5037f1765e2e6701222
Daisuke Hashimoto <14502049+daisuke834@users.noreply.github.com>
2020-07-09 22:22:13 +0900

    LC_weekly_196 LeetCode weekly-196-4. 1505. Minimum Possible Integer After at Most K Adjacent Swaps On Digits

---
+++ src/B_LC_weekly_196_4_segment_tree.h
@@ -0,0 +1,48 @@
+/**
+ * @copyright (c) 2020 Daisuke Hashimoto
+ * @brief LeetCode weekly-196-4. 1505. Minimum Possible Integer After at Most K Adjacent Swaps On Digits
+ * https://leetcode.com/contest/weekly-contest-196/problems/minimum-possible-integer-after-at-most-k-adjacent-swaps-on-digits/
+ */
+
+#ifndef SRC_B_LC_WEEKLY_195_4_TWO_POINTERS_H_
+#define SRC_B_LC_WEEKLY_195_4_TWO_POINTERS_H_
+
+#include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace LC_weekly_196_4 {
+
+class Solution {
+ public:
+  std::string minInteger(std::string num, int k);
+};
+
+class SegmentTree {
+ public:
+  explicit SegmentTree(const int64_t array_size, const int64_t init_value);
+  explicit SegmentTree(const std::vector<int64_t> &array, const int64_t init_value);
+  void Add(const int64_t array_index, const int64_t value);
+  int64_t Find(const int64_t start, const int64_t end);  // [start, end)
+
+ private:
+  SegmentTree() = delete;
+  int64_t LeafNodeIndex(const int64_t array_index) const;
+  static int64_t Parent(const int64_t node_index);
+  static int64_t ChildLeft(const int64_t node_index);
+  static int64_t ChildRight(const int64_t node_index);
+  int64_t Query(const int64_t start, const int64_t end, const int64_t node_index, const int64_t start_of_node,
+                const int64_t end_of_node) const;
+
+ private:
+  int64_t array_size_;
+  int64_t node_size_;
+  const int64_t init_value_;
+  std::vector<int64_t> nodes_;
+};
+
+}  // namespace LC_weekly_196_4
+
+#endif  // SRC_B_LC_WEEKLY_195_4_TWO_POINTERS_H_
