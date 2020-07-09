8fac6af36dc3ee5feebf5fca2b9de83f96807978
Daisuke Hashimoto <14502049+daisuke834@users.noreply.github.com>
2020-07-10 00:14:41 +0900

    misc fix

--- src/segment_tree.h
+++ src/segment_tree.h
@@ -23,7 +23,7 @@ class SegmentTree {
     }
     node_size_ = array_size_ * 2 - 1;
     nodes_ = std::vector<int64_t>(node_size_, init_value_);
-  };
+  }
 
   explicit SegmentTree(const std::vector<int64_t> &array, const int64_t init_value,
                        std::function<int64_t(const int64_t a, const int64_t b)> &&Reduce)
@@ -36,7 +36,7 @@ class SegmentTree {
     }
     node_size_ = array_size_ * 2 - 1;
     nodes_.resize(node_size_, init_value_);
-  };
+  }
 
   void Update(const int64_t array_index, const int64_t value, const bool overwrite = false) {
     int64_t node_index = LeafNodeIndex(array_index);
@@ -49,31 +49,32 @@ class SegmentTree {
       node_index = Parent(node_index);
       nodes_[node_index] = Reduce_(nodes_[ChildLeft(node_index)], nodes_[ChildRight(node_index)]);
     }
-  };
+  }
 
   int64_t Find(const int64_t start, const int64_t end) {
     // [start, end)
     const int64_t result = Query(start, end, 0, 0, array_size_);
     return result;
-  };
+  }
 
  private:
   SegmentTree() = delete;
+
   int64_t LeafNodeIndex(const int64_t array_index) const {
     return array_size_ + array_index - 1L;
-  };
+  }
 
   static int64_t Parent(const int64_t node_index) {
     return (node_index - 1L) / 2L;
-  };
+  }
 
   static int64_t ChildLeft(const int64_t node_index) {
     return (node_index * 2 + 1);
-  };
+  }
 
   static int64_t ChildRight(const int64_t node_index) {
     return (node_index * 2 + 2);
-  };
+  }
 
   int64_t Query(const int64_t start, const int64_t end, const int64_t node_index, const int64_t start_of_node,
                 const int64_t end_of_node) const {
@@ -86,7 +87,7 @@ class SegmentTree {
     const int64_t value_left = Query(start, end, ChildLeft(node_index), start_of_node, middle);
     const int64_t value_right = Query(start, end, ChildRight(node_index), middle, end_of_node);
     return Reduce_(value_left, value_right);
-  };
+  }
 
  private:
   int64_t array_size_;
