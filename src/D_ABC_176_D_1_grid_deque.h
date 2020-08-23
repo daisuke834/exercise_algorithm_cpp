4bbdc1175c828ea5ff428ba664cecb4b9bb2cc61
Daisuke Hashimoto <14502049+daisuke834@users.noreply.github.com>
2020-08-23 10:17:45 +0900

    D_ABC_176_D_1 Wizard in Maze by deque

---
+++ src/D_ABC_176_D_1_grid_deque.h
@@ -0,0 +1,18 @@
+/**
+ * @copyright (c) 2020 Daisuke Hashimoto
+ * @brief AtCoder Beginner Contest(ABC) 176-D. Wizard in Maze.
+ */
+
+#ifndef SRC_D_D_ABC_176_D_1_GRID_DEQUE_H_
+#define SRC_D_D_ABC_176_D_1_GRID_DEQUE_H_
+
+#include <cstdint>
+#include <iostream>
+
+namespace D_ABC_176_D_1 {
+
+int CallMain(std::istream &input_stream);
+
+}  // namespace D_ABC_176_D_1
+
+#endif  // SRC_D_D_ABC_176_D_1_GRID_DEQUE_H_
