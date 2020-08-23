13a6dca93ef839415af2440d1d04de393f0054dd
Daisuke Hashimoto <14502049+daisuke834@users.noreply.github.com>
2020-08-23 10:57:43 +0900

    D_ABC_176_D_2 Wizard in Maze by Dijkstra

---
+++ src/D_ABC_176_D_2_grid_Dijkstra.h
@@ -0,0 +1,18 @@
+/**
+ * @copyright (c) 2020 Daisuke Hashimoto
+ * @brief AtCoder Beginner Contest(ABC) 176-D. Wizard in Maze.
+ */
+
+#ifndef SRC_D_D_ABC_176_D_2_GRID_DIJKSTRA_H_
+#define SRC_D_D_ABC_176_D_2_GRID_DIJKSTRA_H_
+
+#include <cstdint>
+#include <iostream>
+
+namespace D_ABC_176_D_2 {
+
+int CallMain(std::istream &input_stream);
+
+}  // namespace D_ABC_176_D_2
+
+#endif  // SRC_D_D_ABC_176_D_2_GRID_DIJKSTRA_H_
